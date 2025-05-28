928be0abc5b609015d78751a8ac715424c5a5f94
NachoVidal27 <igjovidal@gmail.com>
2025-05-27 23:24:31 -0300

    fixes database connection

--- app/database.py
+++ app/database.py
@@ -3,10 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-
 DATABASE_URL = os.getenv("EXT_DATABASE_URL", "sqlite:///./todos.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
