20259cbe7d77e90ffe115447b2bea678c09191a3
NachoVidal27 <igjovidal@gmail.com>
2025-05-27 23:12:22 -0300

    update database url POSTGRES

--- app/database.py
+++ app/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./todos.db"
+
+DATABASE_URL = os.getenv("EXT_DATABASE_URL", "sqlite:///./todos.db")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
