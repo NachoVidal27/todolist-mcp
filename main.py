b42aba0ff7eebc871881f2f2af248c431917dcb6
NachoVidal27 <igjovidal@gmail.com>
2025-05-27 23:36:54 -0300

    DB setup

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.api import router as api_router
 from app.mcp import router as mcp_router
-
+from app.models import Base
+from app.database import engine
+s
 app = FastAPI(title="TodoList API with MCP Server")
 
 # API principal bajo el prefijo /api
@@ -9,3 +11,5 @@ app.include_router(api_router, prefix="/api")
 
 # MCP Server bajo el prefijo /mcp
 app.include_router(mcp_router, prefix="/mcp")
+
+Base.metadata.create_all(bind=engine)
